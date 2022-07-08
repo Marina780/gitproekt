716c9bdfc3688464ad5a479c973af9e048b1e005
Marina780 <108395227+Marina780@users.noreply.github.com>
2022-07-08 20:59:19 +0300

    Update new.py

--- new.py
+++ new.py
@@ -23,3 +23,21 @@ my_dict={
 # 6  Добавить во множество элемент 9
 # my_dict.setdefault(9)
 # print(my_dict)
+# 8  Удалите из списка элемент 'o'
+# my_dict['moo']={
+#         'e':12,
+#         'f':{'Lol':['L','l']}}
+# print(my_dict)
+
+
+# 9  Добавить в словарь,который явл значением ключа 'f' ключ 'K' со значением ['K', 'e', 'k']
+
+# my_dict['moo']={
+#          'e':12,
+#        'f':{'Lol':['L','l'], 'k':['k','e','k'}}
+# print(my_dict)
+# my_dict['moo']['f']['Lol']={'L','l'},{'k':['k','e','k']}
+# print(my_dict)
+# 10 Очистить словарь my_dict
+# my_dict.clear()
+# print(my_dict)
